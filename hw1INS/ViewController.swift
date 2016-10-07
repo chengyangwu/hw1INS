1962cbfa1c5846f9215fb0eb38c49e05876a7241
chengyangwu <chengyang.wu@gmail.com>
2016-10-07 10:34:57 +0800

    Initial Commit

---
+++ hw1INS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw1INS
+//
+//  Created by 吳承洋 on 2016/10/7.
+//  Copyright © 2016年 edward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
